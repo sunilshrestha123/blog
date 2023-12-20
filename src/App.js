1d946b17de39568e8eca52c8008465520efc25c6
sunilshrestha123 <ilamalisunilshrestha00@gmail.com>
2023-12-20 21:15:42 +0545

    boostrap is added to project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Stack from 'react-bootstrap/Stack';
+import Button from 'react-bootstrap/Button';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>These is my first react app</h1>
+      <Stack direction='horizontal' gap={2}>
+        <Button as='a' variant='primary'>
+          Button as link
+        </Button>
+        <Button as='a' variant='success'>
+          Button as link
+        </Button>
+        <Button as='a' variant='warning'>
+          Button as link
+        </Button>
+      </Stack>
     </div>
   );
-}
+};
 
 export default App;
